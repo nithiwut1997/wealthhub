0ec6196e02cee87b4e8f841ca043ecf9a2033bfc
Nithiwut Wilainuch <nithiwut.w@gmail.com>
2026-07-05 22:29:22 +0700

    feat: initial project

---
+++ src/main/java/com/nithiwut/wealthhub/WealthhubApplication.java
@@ -0,0 +1,13 @@
+package com.nithiwut.wealthhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nithiwut/wealthhub/WealthhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nithiwut.wealthhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
